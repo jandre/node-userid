88240c6107d86a3a9fea2afef394a15078fdf6ee
smart__petea <smart__petea@mail.ru>
2015-08-28 19:36:49 +0300

    uid - test

--- test/ids.js
+++ test/ids.js
@@ -14,4 +14,13 @@ describe("userid", function() {
         libIds.uid.should.equal(shellUid)
         libIds.gid.should.equal(shellGid)
     });
+
+    it("uid", function() {
+        var username = execSync("id -u -n").toString().replace('\n', '');
+
+        var shellUid = execSync("id -u") >> 0;
+        var libUid = userid.uid(username);
+
+        libUid.should.equal(shellUid);
+    });
 });
